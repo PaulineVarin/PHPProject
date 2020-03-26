8ce6b1d7a0c6d194bdd3803760dce48e090794fe
Pauline Varin <pauline.varin@outlook.fr>
2020-03-26 22:15:39 +0100

    fichier create.sql

---
+++ data/create.sql
@@ -0,0 +1,45 @@
+CREATE TABLE IF NOT EXISTS magasin(
+    id_magasin integer PRIMARY KEY,
+    nom text,
+    adresse text,
+    descriptionMagasin text
+);
+
+CREATE TABLE IF NOT EXISTS licence(
+    id_licence integer PRIMARY KEY,
+    nom text
+);
+
+CREATE TABLE IF NOT EXISTS marque(
+    id_marque integer PRIMARY KEY,
+    nom text,
+    descriptionMarque text
+);
+
+CREATE TABLE IF NOT EXISTS typeDeFigurine(
+    id_type integer PRIMARY KEY,
+    nom text,
+    id_marque integer REFERENCES marque(id_marque)
+);
+
+CREATE TABLE IF NOT EXISTS article(
+    reference integer PRIMARY KEY,
+    intitule text,
+    descriptionArticle text,
+    photo text,
+    id_typeFigurine integer REFERENCES typeDeFigurine(id_type),
+    id_licence integer REFERENCES licence(id_licence)
+);
+
+CREATE TABLE IF NOT EXISTS est_disponible(
+    id_article integer REFERENCES article(reference),
+    id_magasin integer REFERENCES magasin(id_magasin),
+    PRIMARY KEY(id_article,id_magasin)
+);
+
+CREATE TABLE IF NOT EXISTS est_associe(
+    id_licence integer REFERENCES licence(id_licence),
+    id_typeFigurine integer REFERENCES typeDeFigurine(id_type),
+    PRIMARY KEY(id_licence,id_typeFigurine)
+);
+
