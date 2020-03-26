c336a61da0ba3680b2d9d61a9bbe54c34a3f6724
Pauline Varin <pauline.varin@outlook.fr>
2020-03-26 23:19:17 +0100

    db correcte

--- data/drop.sql
+++ data/drop.sql
@@ -1,7 +1,7 @@
 DROP TABLE magasin;
 DROP TABLE licence;
 DROP TABLE marque;
-DROP TABLE typeDeFigurinesocie;
+DROP TABLE typeDeFigurine;
 DROP TABLE article;
 DROP TABLE est_disponible;
 DROP TABLE est_associe;
